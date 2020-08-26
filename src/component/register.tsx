c4820b2e4d4da466389823c99f67a65a5bf627e5
kento <kozukata1993@gmail.com>
2020-08-26 20:43:42 +0900

    reduxの基礎導入

--- src/component/register.tsx
+++ src/component/register.tsx
@@ -1,34 +1,10 @@
 import React, { FC } from 'react';
-import { Header, Button } from 'semantic-ui-react';
-import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from '../stores/counter';
-import { Store } from '../interface';
+import { Header } from 'semantic-ui-react';
 
 export const Register: FC = () => {
-  const count = useSelector((store: Store) => store.counter.count);
-  const dispatch = useDispatch();
-
   return (
     <>
-      <Header>Hello</Header>
-      <Header>GitHub Actions Success!</Header>
-      <Header>Layout完成</Header>
-      <Header>Cloud Functions for Firebase </Header>
-      <Header>{count}</Header>
-      <Button
-        positive
-        content="+"
-        onClick={() => {
-          dispatch(increment());
-        }}
-      />
-      <Button
-        negative
-        content="-"
-        onClick={() => {
-          dispatch(decrement());
-        }}
-      />
+      <Header>Register</Header>
     </>
   );
 };
--- src/interface.ts
+++ src/interface.ts
@@ -2,4 +2,11 @@ export interface Store {
   counter: {
     count: number;
   };
+  forecast: {
+    date: Date;
+    summary: string;
+    temperatureMax: number;
+    temperatureMin: number;
+    icon: 'sun' | 'sun' | 'rain' | 'snowflake' | 'cloud' | 'question';
+  };
 }
