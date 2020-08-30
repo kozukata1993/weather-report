2edb909265e2f2873e71c93e35fc833bc25c4fad
kento <kozukata1993@gmail.com>
2020-08-31 07:12:41 +0900

    軽い説明

--- src/component/register/register.tsx
+++ src/component/register/register.tsx
@@ -32,6 +32,12 @@ export const RegisterComponent: React.FC<RegisterProps> = ({
   return (
     <>
       <Header>Register</Header>
+      <p>
+        <a href="https://slack.com/intl/ja-jp/help/articles/115005265063-Slack-%E3%81%A7%E3%81%AE-Incoming-Webhook-%E3%81%AE%E5%88%A9%E7%94%A8">
+          SlackのIncoming Webhook
+        </a>
+        を使って、天気を通知させることができます。
+      </p>
       <RegisterModal
         handleChangeCity={handleChangeCity}
         handleChangeTime={handleChangeTime}
